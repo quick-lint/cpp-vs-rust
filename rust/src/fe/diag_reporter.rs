8d999528c3677964200ad1183ea45276ece7f9cd
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-12-08 22:03:39 -0800

    feat(rust): port null_diag_reporter

--- rust/src/fe/diag_reporter.rs
+++ rust/src/fe/diag_reporter.rs
@@ -9,3 +9,14 @@ pub trait DiagReporter {
 pub fn report<Diag: HasDiagType>(reporter: &dyn DiagReporter, diag: Diag) {
     reporter.report_impl(Diag::TYPE_, &diag as *const Diag as *const u8);
 }
+
+struct NullDiagReporter;
+impl DiagReporter for NullDiagReporter {
+    fn report_impl(&self, _type: DiagType, _diag: *const u8) {}
+}
+
+static NULL_DIAG_REPORTER_SINGLETON: NullDiagReporter = NullDiagReporter;
+
+pub fn null_diag_reporter() -> &'static impl DiagReporter {
+    &NULL_DIAG_REPORTER_SINGLETON
+}
