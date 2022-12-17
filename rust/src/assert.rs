07271198d7ae823cc89c21841a700bf241ad0c13
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-12-17 01:00:34 -0800

    feat(rust): show source location on narrow_cast failure

--- rust/src/assert.rs
+++ rust/src/assert.rs
@@ -6,7 +6,7 @@ use crate::util::c_string::*;
 macro_rules! qljs_always_assert {
     ($cond:expr $(,)?) => {
         if !$cond {
-            $crate::assert::report_assertion_failure(
+            $crate::assert::report_assertion_failure_c(
                 /*file_name=*/ $crate::qljs_c_string!(file!()),
                 /*line=*/ line!(),
                 /*message=*/ $crate::qljs_c_string!(stringify!($cond)),
@@ -47,15 +47,19 @@ macro_rules! qljs_assert_trap {
 }
 
 // NOTE(strager): We prefer raw pointers to reduce code bloat when calling the assertion failure
-// function. Perhaps we should look at other solutions.
-pub fn report_assertion_failure(file_name: *const u8, line: u32, message: *const u8) {
+// function. Perhaps we should look at other solutions, such as std::panic::Location.
+pub fn report_assertion_failure_c(file_name: *const u8, line: u32, message: *const u8) {
     unsafe {
         let file_name: &str = read_utf8_c_string(file_name);
         let message: &str = read_utf8_c_string(message);
-        eprintln!(
-            "{file_name}:{line}: internal check failed: {message}\n\
+        report_assertion_failure(file_name, line, message);
+    }
+}
+
+pub fn report_assertion_failure(file_name: &str, line: u32, message: &str) {
+    eprintln!(
+        "{file_name}:{line}: internal check failed: {message}\n\
                quick-lint-js crashed. Please report this bug here:\n\
                https://quick-lint-js.com/crash-report/\n"
-        );
-    }
+    );
 }
