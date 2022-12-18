bdf31559b877987347f0fb8b525b8d81b8f3373b
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-12-17 21:47:49 -0800

    fix(rust): avoid unaligned data reads in BufferingDiagReporter

--- rust/src/fe/buffering_diag_reporter.rs
+++ rust/src/fe/buffering_diag_reporter.rs
@@ -3,6 +3,7 @@ use crate::fe::diag_reporter::*;
 use crate::fe::diagnostic_types::*;
 use crate::port::allocator::*;
 use crate::port::maybe_uninit::*;
+use crate::qljs_const_assert;
 
 pub struct BufferingDiagReporter<'alloc, 'code> {
     diagnostics: std::cell::UnsafeCell<LinkedVector<'alloc, StoredDiag<'code>>>,
@@ -18,7 +19,7 @@ impl<'alloc, 'code> BufferingDiagReporter<'alloc, 'code> {
     pub fn copy_into(&self, other: &dyn DiagReporter) {
         unsafe { &mut *self.diagnostics.get() }.for_each(|diag: &StoredDiag| {
             // TODO(strager): Make report_impl accept a const pointer to reduce casting.
-            other.report_impl(diag.type_, &diag.diag as *const _ as *mut u8);
+            other.report_impl(diag.type_, &diag.diag.data as *const _ as *mut u8);
         });
     }
 
@@ -47,14 +48,26 @@ impl<'alloc, 'code> DiagReporter for BufferingDiagReporter<'alloc, 'code> {
         }
         unsafe { &mut *self.diagnostics.get() }.push(StoredDiag {
             type_: type_,
-            diag: diag_data,
-            phantom: std::marker::PhantomData,
+            diag: StoredDiagData {
+                data: diag_data,
+                phantom: std::marker::PhantomData,
+            },
         });
     }
 }
 
 struct StoredDiag<'code> {
     type_: DiagType,
-    diag: [std::mem::MaybeUninit<u8>; MAX_SIZE_OF_DIAGNOSTIC_TYPE],
+    diag: StoredDiagData<'code>,
+}
+
+#[repr(align(8))]
+struct StoredDiagData<'code> {
+    data: [std::mem::MaybeUninit<u8>; MAX_SIZE_OF_DIAGNOSTIC_TYPE],
     phantom: std::marker::PhantomData<&'code u8>,
 }
+
+qljs_const_assert!(
+    std::mem::align_of::<StoredDiagData>() >= std::mem::align_of::<AnyDiag>(),
+    "StoredDiag should be aligned such that it can be cast to any diagnostic struct",
+);
