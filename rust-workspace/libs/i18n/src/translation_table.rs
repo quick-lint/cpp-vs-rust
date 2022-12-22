e55ac90d4e7f6f1e403bde741f11d929db9cf622
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-12-21 19:17:56 -0800

    refactor(rust-workspace): split code into multiple crates

---
+++ rust-workspace/libs/i18n/src/translation_table.rs
@@ -0,0 +1,19 @@
+use crate::translation_table_generated::*;
+use cpp_vs_rust_container::sorted_search::*;
+
+pub const TRANSLATION_TABLE_UNALLOCATED_MAPPING_INDEX: u16 = 0;
+
+pub struct TranslationTableMappingEntry(
+    // string offsets
+    pub [u32; (TRANSLATION_TABLE_LOCALE_COUNT + 1) as usize],
+);
+
+pub const fn translation_table_const_look_up(untranslated: &str) -> u16 {
+    let index: Option<usize> = sorted_search(&UNTRANSLATED_STRINGS, untranslated);
+    match index {
+        Some(index) => (index + 1) as u16,
+        None => {
+            panic!("translation_table_generated.rs is out of date. Run tools/update-translator-sources to rebuild this file.");
+        }
+    }
+}
--- rust-workspace/src/container/monotonic_allocator.rs
+++
@@ -1,4 +0,0 @@
-use crate::container::linked_bump_allocator::*;
-
-const ALIGN_OF_POINTER: usize = std::mem::align_of::<usize>();
-pub type MonotonicAllocator = LinkedBumpAllocator<ALIGN_OF_POINTER>;
