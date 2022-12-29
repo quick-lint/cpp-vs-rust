faca92f1478a7e8f971a9da2a2965d7ce3d3fd06
Matthew "strager" Glazar <strager.nds@gmail.com>
2022-12-28 23:10:27 -0800

    feat(rust): port web-demo-location

---
+++ rust-workspace/libs/c_api/src/web_demo_location.rs
@@ -0,0 +1,36 @@
+use cpp_vs_rust_fe::source_code_span::*;
+use cpp_vs_rust_util::narrow_cast::*;
+use cpp_vs_rust_util::padded_string::*;
+use cpp_vs_rust_util::utf_8::*;
+
+pub type WebDemoSourceOffset = u32;
+
+pub struct WebDemoSourceRange {
+    pub begin: WebDemoSourceOffset,
+    pub end: WebDemoSourceOffset,
+}
+
+pub struct WebDemoLocator<'a> {
+    input: PaddedStringView<'a>,
+}
+
+impl<'a> WebDemoLocator<'a> {
+    pub fn new(input: PaddedStringView<'a>) -> WebDemoLocator<'a> {
+        WebDemoLocator { input: input }
+    }
+
+    pub fn range(&self, span: SourceCodeSpan<'_>) -> WebDemoSourceRange {
+        WebDemoSourceRange {
+            begin: self.position(span.begin_ptr()),
+            end: self.position(span.end_ptr()),
+        }
+    }
+
+    pub fn position(&self, c: *const u8) -> WebDemoSourceOffset {
+        let byte_offset: i32 = narrow_cast::<i32, _>(unsafe { c.offset_from(self.input.c_str()) });
+        narrow_cast::<WebDemoSourceOffset, _>(count_lsp_characters_in_utf_8(
+            self.input,
+            byte_offset,
+        ))
+    }
+}
