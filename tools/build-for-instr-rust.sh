95e0e0492eac98f1c128bd73e0e61f525acfce89
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-01-04 01:23:14 -0800

    feat(build-toolchain): increase code coverage for Rust PGO

--- tools/build-for-instr-rust.sh
+++ tools/build-for-instr-rust.sh
@@ -9,14 +9,21 @@ build_dir() {
     local dir="${1}"
     (
         cd "${dir}"
-        cargo clean --profile quick-build-incremental
-        cargo test --profile quick-build-incremental
-        cargo clean --profile quick-build-incremental
-        cargo build --profile quick-build-incremental
-        cargo test --profile quick-build-incremental
+
+        for profile in quick-build-incremental quick-build-nonincremental; do
+            cargo clean --profile "${profile}"
+            cargo test --profile "${profile}"
+            cargo clean --profile "${profile}"
+            cargo build --profile "${profile}"
+            cargo test --profile "${profile}"
+        done
     )
 }
 
 build_dir rust
+build_dir rust-threecrate-cratecargotest
+build_dir rust-threecrate-crateunotest
 build_dir rust-twocrate-cratecargotest
+build_dir rust-twocrate-unittest
+build_dir rust-workspace-cratecargotest-nodefaultfeatures
 build_dir rust-workspace-crateunotest
