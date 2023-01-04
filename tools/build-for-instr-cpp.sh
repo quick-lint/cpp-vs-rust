b3c63d52f5412b8def2497503048fd3b2ed013e8
Matthew "strager" Glazar <strager.nds@gmail.com>
2023-01-03 21:12:48 -0800

    feat(build-toolchains): profile build with libstdc++ both libc++
    Don't bias against one or the other.

--- tools/build-for-instr-cpp.sh
+++ tools/build-for-instr-cpp.sh
@@ -5,19 +5,21 @@ set -u
 
 cd "$(dirname "${0}")/../cpp"
 
-rm -rf build
-LDFLAGS=-fuse-ld=lld \
-    cmake \
-    -DCMAKE_C_COMPILER=clang \
-    -DCMAKE_CXX_COMPILER=clang++ \
-    -DCMAKE_LINKER="$(which lld)" \
-    -DCMAKE_AR="$(which llvm-ar)" \
-    -DCMAKE_RANLIB="$(which llvm-ranlib)" \
-    -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_CXX_FLAGS='-g0' \
-    -G Ninja -S . -B build
-ninja -C build
-for _ in {1..4}; do
-    find build/{src,test}/ -name '*.cpp.o' -delete
-    ninja -C build
+for stdlib in libc++ libstdc++; do
+  rm -rf build
+  LDFLAGS=-fuse-ld=$(which mold) \
+      cmake \
+      -DCMAKE_C_COMPILER=clang \
+      -DCMAKE_CXX_COMPILER=clang++ \
+      -DCMAKE_LINKER="$(which mold)" \
+      -DCMAKE_AR="$(which llvm-ar)" \
+      -DCMAKE_RANLIB="$(which llvm-ranlib)" \
+      -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_CXX_FLAGS="-g0 -fpch-instantiate-templates -stdlib=${stdlib}" \
+      -G Ninja -S . -B build
+  ninja -C build
+  for _ in {1..4}; do
+      find build/{src,test}/ -name '*.cpp.o' -delete
+      ninja -C build
+  done
 done
